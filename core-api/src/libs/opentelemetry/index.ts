6ae68dcdb3c67adb5d67359dc98102eb4d890190
theo <jeangho293@naver.com>
2024-05-09 10:28:53 +0900

    fix: file name:

--- core-api/src/libs/opentelemetry/index.ts
+++ core-api/src/libs/opentelemetry/index.ts
@@ -2,8 +2,17 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { Resource } from "@opentelemetry/resources";
+import {
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_VERSION,
+} from "@opentelemetry/semantic-conventions";
 
 const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: "core-api",
+    [SEMRESATTRS_SERVICE_VERSION]: "1.0",
+  }),
   traceExporter: new OTLPTraceExporter({
     url: "http://localhost:4318/v1/traces",
     headers: {},
